25662d1fe80abc87b08e53627f4a191ce8469b50
nahK994 <nkskl6@gmail.com>
2024-08-05 13:52:59 +0600

    update

--- main.go
+++ main.go
@@ -27,13 +27,16 @@ func (s *Server) acceptLoop() {
 			continue
 		}
 
-		fmt.Println("new connection to", conn.RemoteAddr())
+		msg := fmt.Sprintln("new connection to", conn.RemoteAddr())
+		conn.Write([]byte(msg))
 		go s.readLoop(conn)
 	}
 }
 
 func (s *Server) readLoop(conn net.Conn) {
 	buf := make([]byte, 2048)
+	msg := fmt.Sprintf("%s: ", conn.RemoteAddr())
+	conn.Write([]byte(msg))
 	for {
 		n, err := conn.Read(buf)
 		if err != nil {
@@ -41,9 +44,10 @@ func (s *Server) readLoop(conn net.Conn) {
 			break
 		}
 
-		msg := buf[:n]
-		fmt.Printf("%s: %s\n", conn.RemoteAddr(), string(msg))
-		conn.Write([]byte(fmt.Sprintf("Received --> %s\n", string(msg))))
+		fmt.Printf("%s: %s", conn.RemoteAddr(), string(buf[:n]))
+		receivedMsg := fmt.Sprintf("Received --> %s", string(buf[:n]))
+		conn.Write([]byte(receivedMsg))
+		conn.Write([]byte(msg))
 	}
 }
 
@@ -62,6 +66,7 @@ func (s *Server) Start() error {
 }
 
 func main() {
+	fmt.Println("Server started")
 	server := NewServer(":8000")
 	log.Fatal(server.Start())
 }
