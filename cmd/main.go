fdd74749b90ef285868e51ff0cb8511b1632ae27
nahK994 <nkskl6@gmail.com>
2024-08-21 14:51:11 +0600

    create cmd package

--- cmd/main.go
+++ cmd/main.go
@@ -10,10 +10,16 @@ import (
 
 func main() {
 	srv := server.InitiateHttp("127.0.0.1:8000")
-	srv.RequestHandler("/login", "POST", func(r models.Request, w *models.Response) {
+	srv.RequestHandler("/post", "POST", func(r models.Request, w *models.Response) {
 		fmt.Println("TEST ===>", r.Body)
 		w.StatusCode = 201
 		w.Body = r.Body
+	})
+
+	srv.RequestHandler("/get", "GET", func(r models.Request, w *models.Response) {
+		fmt.Println("TEST ===>", r.Body)
+		w.StatusCode = 200
+		w.Body = "Hello World!!"
 	})
 	log.Fatal(srv.Start())
 }
